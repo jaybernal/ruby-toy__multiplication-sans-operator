384c8e526665cb28ec4a66610e907b52fb026692
jaybernal <jjbernal@gmail.com>
2016-02-26 19:51:43 +0000

    done

--- lib/multiply.rb
+++ lib/multiply.rb
@@ -1,2 +1,10 @@
 def multiply(num1, num2)
+i = 0 # counter
+j = 0 # placeholder for total
+    while i < num1 # if i is less then num1, run loop
+        j = j + num2 # add the solution together
+        i+=1 # count up to num1
+    end
+    return j #print answer
 end
+multiply(2,8)
